c05c6ef9ff92e62bd8b9896c073c8037c574a969
Enzo Zamineli <enzozamineli@gmail.com>
2025-08-29 08:54:13 -0300

    configurando firebase-auth

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") 
 }
 
 android {
@@ -41,4 +42,15 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+// Firebase BoM (sempre mantenha atualizado)
+    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+
+    // Dependências Firebase (sem versão, herdam do BoM)
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-analytics") 
+    // outros serviços que você precisar
 }
